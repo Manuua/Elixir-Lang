01769be517441d3d3debf7dc4c45fe5fdf292166
Manoela Andrade <manoelaandrade@MacBook-Air-de-Manoela.local>
2024-01-11 23:57:59 -0300

    feat: first for in elixir

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,10 +1,19 @@
 defmodule Cards do
 
   def create_deck do
-    ["Ace", "Two", "Three"]
+   values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
+
+    for suit <- suits do
+      "is it a for in elixir?"
+    end
   end
 
   def shuffle(deck) do
   Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 end
